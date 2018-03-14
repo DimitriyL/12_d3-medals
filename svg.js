9a6cba89b25d213c11236f835ef593cbbafb97b4
sonalparab <sparab@stuy.edu>
2018-03-13 23:39:57 -0400

    js link

--- svg.js
+++ svg.js
@@ -1,6 +1,7 @@
 var pic = document.getElementById("pic");
 var switchButt = document.getElementById("switch");
-var isGermany = True;
+var country = document.getElementById("country");
+var isGermany = true;
 
 var Germany = [14,10,7];
 var Canada = [11,8,10];
@@ -8,17 +9,17 @@ var Canada = [11,8,10];
 var display = function(){
     var gold = document.createElementNS("http://www.w3.org/2000/svg","circle");
     gold.setAttribute("cx", 100);
-    gold.setAttribute("cy", 50);
+    gold.setAttribute("cy", 100);
     gold.setAttribute("r",10);
     gold.setAttribute("fill", "goldenrod");
     var silver = document.createElementNS("http://www.w3.org/2000/svg","circle");
     silver.setAttribute("cx", 100);
-    silver.setAttribute("cy", 150);
+    silver.setAttribute("cy", 250);
     silver.setAttribute("r",10);
     silver.setAttribute("fill", "lightsteelblue");
     var bronze = document.createElementNS("http://www.w3.org/2000/svg","circle");
     bronze.setAttribute("cx", 100);
-    bronze.setAttribute("cy", 250);
+    bronze.setAttribute("cy", 400);
     bronze.setAttribute("r",10);
     bronze.setAttribute("fill", "orange");
     
@@ -29,22 +30,26 @@ var display = function(){
 
 var medalize = function(e){
     var medals;
-    if(isGermany == True){
-	medals = d3.selectAll("circle").data(Germany);
+    if(isGermany == true){
+	medals = d3.selectAll("circle")
+	medals.data(Germany);
     }
     else{
 	medals = d3.selectAll("circle").data(Canada);
     }
-    medals.attr("r", function(d){return d*10;});
+    medals.attr("r", function(d){return d*5;});
 }
 
 var switchFxn = function(e){
-    if(isGermany == True){
-	isGermany = False;
+    if(isGermany == true){
+	isGermany = false;
+	country.innerHTML = "Canada";
     }
     else{
-	isGermany = True;
+	isGermany = true;
+	country.innerHTML = "Germany";
     }
+    medalize();
 }
 
 switchButt.addEventListener("click", switchFxn);
